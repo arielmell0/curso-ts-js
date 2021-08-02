6fe778031a5a011fa3e1fbd45d3236cfe1eef33c
arielmell0 <arielmello@rede.ulbra.br>
2021-08-02 14:20:20 -0300

    refactor: :sparkles: organized file structure and added for-classic loop example

---
+++ 19.for-classic.js
@@ -0,0 +1,7 @@
+const myArray = ['ovo', 'farinha', 'cobertura', 'cereja']
+
+//index = indíce, ele comeã em 0 (index = 0) e vai aumentando(index++) conforme for menor ou igual ao
+//tamanho do lista(index <= myArray.length)
+for(let index = 0; index < myArray.length; index++) {
+    console.log(myArray[index])
+}

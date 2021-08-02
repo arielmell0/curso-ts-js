252c241294671c17dd0123f12b32788b0e3b0f28
arielmell0 <arielmello@rede.ulbra.br>
2021-08-02 20:56:01 -0300

    feat: :sparkles: while and do while loop

---
+++ 3. JavaScript - Lógica de programação/23. While e Do While.js
@@ -0,0 +1,18 @@
+let i = 0
+let doI = 0
+
+while(i <= 5) {
+    console.log(i)
+    //sempre adição para evitar loops 
+    i++
+}
+
+console.log('###@###')
+
+//faça(do) enquanto(while), executa no minímo uma vez o do e dps verifica a condição
+do {
+    console.log(doI)
+    doI++
+} while(doI <= 5) {
+    doI++
+}

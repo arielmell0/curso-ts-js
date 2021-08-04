43657284c3683a222ca348cbee290ee340046218
arielmell0 <arielmello@rede.ulbra.br>
2021-08-04 14:28:10 -0300

    refactor: :zap: improving the code

--- 3. JavaScript - Lógica de programação/25. Exercício com lógica de programação - 01.js
+++ 3. JavaScript - Lógica de programação/25. Exercício com lógica de programação - 01.js
@@ -2,8 +2,6 @@
 
 //Escreva uma função que receba dois números e retorne o maior deles
 
-function nBigger(n1, n2) {
-    return n1 > n2 ? n1 : n2
-}
+const nBigger = (n1, n2) => n1 > n2 ? n1 : n2
 
 console.log(nBigger(2, 5))

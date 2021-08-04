c21707b6dfdd2a8304f8f2df87456d35e28a5df7
arielmell0 <arielmello@rede.ulbra.br>
2021-08-04 14:54:11 -0300

    feat: :sparkles: exercise if are retract

---
+++ 3. JavaScript - Lógica de programação/26. Exercício com lógica de programação - 02.js
@@ -0,0 +1,10 @@
+// Escreva uma função chamada ePaisagem que
+// que recebe dois argumentos, largura e altura
+// de uma imagem (number).
+// Retorne true se a imagem estiver no modo 
+// paisagem.
+
+const ePaisagem = (largura, altura) => true ? largura > altura : false
+
+console.log(ePaisagem(1980, 1080))
+

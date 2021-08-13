983b2de4013280b732cae585fc02c4e5f61cbea0
arielmell0 <arielmello@rede.ulbra.br>
2021-08-12 22:04:48 -0300

    feat: added state variable and reset event
    state variable contains the state of the clock (started, paused) and reset function reset the seconds variable to zero

--- 3. JavaScript - Lógica de programação/31. Exercício - Criando um timer com setInterval/assets/js/main.js
+++ 3. JavaScript - Lógica de programação/31. Exercício - Criando um timer com setInterval/assets/js/main.js
@@ -1,10 +1,10 @@
 const relogio = document.querySelector('#clock')
-
 const iniciar = document.querySelector('#start')
 const pausar = document.querySelector('#pause')
 const zerar = document.querySelector('#reset')
 
-let seconds = 1;
+let seconds = 0;
+let clockState
 
 function clock(seconds) {
     let time = new Date(seconds * 1000)
@@ -16,20 +16,23 @@ function clock(seconds) {
 }
 
 relogio.addEventListener('click', function (event) {
-    relogio.innerHTML = clock(0)
+    
 })
 
 iniciar.addEventListener('click', function (event) {
-    setInterval(function () {
-        relogio.innerHTML = clock(seconds) 
+    clockState = setInterval(function () {
         seconds++
+        relogio.innerHTML = clock(seconds) 
     }, 1000)
 })
 
 pausar.addEventListener('click', function (event) {
-    alert('Clickei no pausar!')
+    setTimeout(function () {
+        clearInterval(clockState)
+    }, 0)
 })
 
 zerar.addEventListener('click', function(event) {
-    alert('Clickei no zerar!')
+    seconds = 0
+    relogio.innerHTML = clock(seconds)
 })

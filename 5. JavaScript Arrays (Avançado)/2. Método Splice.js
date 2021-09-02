c94b2ff66a32d16b3a138ebab651aed621624323
arielmell0 <arielmello@rede.ulbra.br>
2021-09-02 17:56:14 -0300

    feat: more splice

--- 5. JavaScript Arrays (Avançado)/2. Método Splice.js
+++ 5. JavaScript Arrays (Avançado)/2. Método Splice.js
@@ -1,9 +1,13 @@
+//                                 -2          -1
 //                0        1        2           3 
 const names = ['Maria', 'João', 'Eduardo', 'Gabriel']
 
 // names.splice(índice, delete, elemen1, elemen2, elemen3)
 // pop
-const removeds = names.splice(2, 2)
+
+// Começa do -2 ou seja 0 -2 elementos do fim do array
+// Number.MAX_VALUE representa o maior número do javascript
+const removeds = names.splice(-2, Number.MAX_VALUE) 
 console.log(names, removeds)
 
 // output -> [ 'Maria', 'João' ] [ 'Eduardo', 'Gabriel' ]

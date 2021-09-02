2242b95625b067b64c36d57a98a1e9d8b978220a
arielmell0 <arielmello@rede.ulbra.br>
2021-09-02 16:45:04 -0300

    feat: array passed how reference

--- 5. JavaScript Arrays (Avançado)/1. Revisão do básico em Arrays.js
+++ 5. JavaScript Arrays (Avançado)/1. Revisão do básico em Arrays.js
@@ -1,3 +1,8 @@
-//                          0         1        2
-const names = new Array('Eduardo', 'Maria', 'Joana')
-// usando construtor para criar arrays
+//                 0         1        2
+const names = ['Eduardo', 'Maria', 'Joana']
+const names2 = names
+
+names.pop()
+// Como names está sendo passado só como referência para names2
+// Todas as alterações em um são refletidas no outro
+console.log(names)

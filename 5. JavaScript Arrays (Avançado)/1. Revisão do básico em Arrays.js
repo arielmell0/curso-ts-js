79005673e4186ddf9720cd5dffff26546219a7a8
arielmell0 <arielmello@rede.ulbra.br>
2021-09-02 17:04:29 -0300

    feat: array methods

--- 5. JavaScript Arrays (Avançado)/1. Revisão do básico em Arrays.js
+++ 5. JavaScript Arrays (Avançado)/1. Revisão do básico em Arrays.js
@@ -3,3 +3,21 @@ const names = ['Eduardo', 'Maria', 'Joana']
 const names2 = [...names]
 // Spread operator copia os valores em vez de somente passar
 // Como referência
+
+// métodos de array:
+
+// shift -> remove o primeiro item e realoca os indíces
+// unshift -> adiciona algo ao primeiro item e realoca os indíces
+
+// push -> adiciona um elemento no fim do array
+// pop -> remove um elemento do fim do array
+
+// método split():
+// const nome = 'Ariel Oliveira de Mello'
+// const nomes = nome.split(' ')
+// output -> ['Ariel', 'Oliveira', 'de', 'Mello']
+
+// método join():
+// const nomes = ['Ariel', 'Oliveira', 'de', 'Mello']
+// const nome = nomes.join(' ')
+// output -> Ariel Oliveira de Mello

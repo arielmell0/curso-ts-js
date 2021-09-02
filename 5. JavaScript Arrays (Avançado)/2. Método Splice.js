b5891f44f2fe5e209894aad67f0324e7cdc94331
arielmell0 <arielmello@rede.ulbra.br>
2021-09-02 18:13:24 -0300

    feat: unshift with splice

--- 5. JavaScript Arrays (Avançado)/2. Método Splice.js
+++ 5. JavaScript Arrays (Avançado)/2. Método Splice.js
@@ -4,8 +4,8 @@ const names = ['Maria', 'João', 'Eduardo', 'Gabriel']
 
 // names.splice(índice, delete, elemen1, elemen2, elemen3)
 
-// push com splice()
-names.splice(names.length, 0, 'Ariel', 'Adriana')
+// unshift com splice
+names.splice(0, 0, 'Nicolas', 'Gustavo')
 console.log(names)
 
 // output -> [ 'Maria', 'João', 'Eduardo', 'Gabriel', 'Ariel' ]

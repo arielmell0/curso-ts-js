53a8217ff67a64b1441847f44e1cff6b2cdc2f1e
arielmell0 <arielmello@rede.ulbra.br>
2021-09-02 20:24:05 -0300

    feat: filter example
    full callback function of filter to exemplify

--- 5. JavaScript Arrays (Avançado)/4. Filter - Filtrando o array.js
+++ 5. JavaScript Arrays (Avançado)/4. Filter - Filtrando o array.js
@@ -1,15 +1,10 @@
 // retorne os números maiores que 10
 const numbers = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
-const biggerThan10 = []
 
-function biggerThan(array, number) {
-    for(let i of array) {
-        if(i > number) {
-            biggerThan10.push(i)
-        }
-    }
+function callBackFilter(value, index, array) {
+    return value > 10
 }
 
-biggerThan(numbers, 10)
+const filteredNumbers = numbers.filter(callBackFilter)
 
-console.log(biggerThan10)
+console.log(filteredNumbers)

f13ec3174da5e22990cbaaa918e52203f02c8436
arielmell0 <arielmello@rede.ulbra.br>
2021-09-02 23:16:40 -0300

    feat: filter complete with examples

--- 5. JavaScript Arrays (Avançado)/4. Filter - Filtrando o array.js
+++ 5. JavaScript Arrays (Avançado)/4. Filter - Filtrando o array.js
@@ -1,10 +1,31 @@
 // retorne os números maiores que 10
 const numbers = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
 
-function callBackFilter(value, index, array) {
-    return value > 10
-}
+// quando a função tem só uma linha o retorno é implicito
+const filteredNumbers = numbers.filter(value => value > 10) 
 
-const filteredNumbers = numbers.filter(callBackFilter)
+//console.log(filteredNumbers)
+// output -> [ 50, 80, 11, 15, 22, 27 ]
 
-console.log(filteredNumbers)
+const people = [
+    { name: 'Luiz', age: 62},
+    {name: 'Maria', age: 23},
+    {name: 'Eduardo', age: 55},
+    {name: 'Letícia', age: 19},
+    {name: 'Rosana', age: 32},
+    {name: 'Wallace', age: 47}
+]
+
+const peopleWithLargeName = people.filter (obj => obj.name.length >= 5)
+const peopleWithMoreThan50Years = people.filter(obj => obj.age > 50)
+const nameEndsWithA = people.filter(obj => {
+    return obj.name.toLowerCase().endsWith('a')
+})
+
+console.log(nameEndsWithA)
+// output -> 
+// [
+//   { name: 'Maria', age: 23 },
+//   { name: 'Letícia', age: 19 },
+//   { name: 'Rosana', age: 32 }
+// ]

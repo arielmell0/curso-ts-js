e1696c020ef803c89d99fe57125a2f312286ea57
arielmell0 <arielmello@rede.ulbra.br>
2021-10-12 22:04:35 -0300

    feat: Object.defineProperties
    define mais de uma propriedade utilizando chaves

--- 6. JavaScript Objetos e prototypes (Avançado)/2. Object.defineProperty() e Object.defineProperties().js
+++ 6. JavaScript Objetos e prototypes (Avançado)/2. Object.defineProperty() e Object.defineProperties().js
@@ -1,6 +1,7 @@
 const CriaProduto = function(nome, preco, estoque) {
     this.nome = nome,
     this.preco = preco,
+    // definir propriedades com mais precisão
     Object.defineProperty(this, 'estoque', {
         enumerable:true,  // mostra a chave (mostra o valor no for)
         value: estoque, // valor
@@ -9,6 +10,30 @@ const CriaProduto = function(nome, preco, estoque) {
         // configurável: posso declarar outro Object.defineProperty
         // e alterar as configurações do Object.defineProperty atual
         // caso false tbm não posso apagar a propriedade
+    })
+
+    // para mais de uma propriedade: 
+    Object.defineProperties(this, {
+        nome2: {
+            enumerable: true,
+            value: 'Camiseta manga curta',
+            writable: true,
+            configurable: true
+        },
+
+        preco2: {
+            enumerable: true,
+            value: 39.49,
+            writable: true,
+            configurable: true
+        },
+
+        estoque2: {
+            enumerable: true,
+            value: 5,
+            writable: true,
+            configurable: true
+        }
     })
 }
 

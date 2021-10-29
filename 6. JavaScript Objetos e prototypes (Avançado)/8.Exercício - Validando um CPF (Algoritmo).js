9dd6c0b9b0b81456d72f0ec1e88cbc2dadde9db5
arielmell0 <arielmello@rede.ulbra.br>
2021-10-29 12:44:38 -0300

    feat: transformando  a string em array
    transformando a string cpf para em um array

--- 6. JavaScript Objetos e prototypes (Avançado)/8.Exercício - Validando um CPF (Algoritmo).js
+++ 6. JavaScript Objetos e prototypes (Avançado)/8.Exercício - Validando um CPF (Algoritmo).js
@@ -4,6 +4,10 @@ const limpaCpf = function(cpf) {
     return cpfLimpo
 }
 
-console.log(limpaCpf('920.432.123-44'))
+const stringToArray = function(limpaCpf) {
+    return (Array.from(limpaCpf))
+}
+
+console.log(stringToArray(limpaCpf('920.432.123-44')))
 // output ->
-// 92043212344
+// [ '9', '2', '0', '4', '3', '2', '1', '2', '3', '4', '4' ]

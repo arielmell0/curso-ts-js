e02fd6d7a7c7de0c0f978ba66abae76161eea94a
arielmell0 <arielmello@rede.ulbra.br>
2021-11-18 23:56:24 -0300

    feat: problema de ordenação resolvido
    problema resolvido utilizando o objeto Map()

--- 6. JavaScript Objetos e prototypes (Avançado)/11. Objeto Map().js
+++ 6. JavaScript Objetos e prototypes (Avançado)/11. Objeto Map().js
@@ -17,3 +17,21 @@ const pessoas = [
 //   '3': { id: 3, nome: 'Ariel' } }
 // o javascript muda a ordem de saida dos elementos para ordem numerica
 // usando o objeto Map() vc evita o problema da troca de ordem
+
+const novasPessoas = new Map()
+for (const pessoa of pessoas) {
+    const {id} = pessoa
+    // set para definir 
+    novasPessoas.set(id, {...pessoa})
+}
+
+// com o método keys retorna só as chaves e com o método
+// value retorna todos os valores
+// for(const pessoas of novasPessoas.value()) {
+//     console.log(pessoas)
+// }
+
+// deletando o id 2
+novasPessoas.delete(2)
+console.log(novasPessoas)
+

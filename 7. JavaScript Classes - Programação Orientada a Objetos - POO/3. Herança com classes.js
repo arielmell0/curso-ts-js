f1d58e3d8c7931b4afd9e2b08accdeeb7c760053
arielmell0 <arielmello@rede.ulbra.br>
2021-12-13 23:04:59 -0300

    feat: herança em javascript
    métodos Super e mais

--- 7. JavaScript Classes - Programação Orientada a Objetos - POO/3. Herança com classes.js
+++ 7. JavaScript Classes - Programação Orientada a Objetos - POO/3. Herança com classes.js
@@ -1,5 +1,69 @@
 class DispositivoEletronico {
     constructor(nome) {
         this.nome = nome
+        this.ligado = false
+    }
+
+    ligar() {
+        if(this.ligado) {
+            console.log(this.nome + ' já está ligado.')
+            return
+        }
+
+        this.ligado = true;
+    }
+
+    desligar() {
+        if(!this.ligado) {
+            console.log(this.nome + ' já desligado')
+            return
+        }
+
+        this.ligado = false
     }
 }
+
+class Smartphone extends DispositivoEletronico {
+    constructor(nome, cor, modelo) {
+        // herda o método construtor do pai 
+        super(nome)
+
+        this.cor = cor
+        this.modelo = modelo
+    }
+}
+
+class Tablet extends DispositivoEletronico {
+    constructor(nome, temWifi) {
+        super(nome)
+
+        this.temWifi = temWifi
+    }
+
+    ligar() {
+        console.log('Olha, você alterou o método ligar.')
+    }
+
+    falaOi() {
+        console.log('Oi')
+    }
+}
+
+const s1 = new Smartphone('Samsung', 'Preto', 'Galaxy Note 10')
+console.log(s1)
+// output -> 
+// Smartphone {
+//     nome: 'Samsung',
+//     ligado: false,
+//     cor: 'Preto',
+//     modelo: 'Galaxy Note 10'
+//   }
+
+const t1 = new Tablet('iPad', true)
+t1.ligar()
+t1.ligar()
+t1.falaOi()
+// output ->
+// Olha, você alterou o método ligar.
+// Olha, você alterou o método ligar.
+// Oi

99f8060134ae7190b4f13d9cc1f98b83547f0692
arielmell0 <arielmello@rede.ulbra.br>
2021-12-14 22:24:55 -0300

    feat: método estático exemplo

--- 7. JavaScript Classes - Programação Orientada a Objetos - POO/4. Métodos de instância e estáticos.js
+++ 7. JavaScript Classes - Programação Orientada a Objetos - POO/4. Métodos de instância e estáticos.js
@@ -4,20 +4,31 @@ class ControleRemoto {
         this.volume = 0
     }
 
+    // método de instância
     aumentarVolume() {
         if(this.volume >= 100) return
         this.volume += 2
     }
 
+    // método de instância
     diminuirVolume() {
         if(this.volume <= 0) return
         this.volume -= 2
     }
+
+    // método estático
+    static trocaPilha() {
+        console.log('Ok, vou trocar.')
+    }
 }
 
 const controle1 = new ControleRemoto('LG')
+// métodos de instância
 controle1.aumentarVolume()
 controle1.aumentarVolume()
+
+// método estáticos
+ControleRemoto.trocaPilha()
 
 console.log(controle1)
 // output ->

5e6aeed08efefae233f2abe9e76603effd1ad09c
arielmell0 <arielmello@rede.ulbra.br>
2021-12-17 22:22:12 -0300

    feat: classe válida cpf terminada
    classe responsável pela validação de cpf de usuários

--- 7. JavaScript Classes - Programação Orientada a Objetos - POO/5. Usando classes (Validando CPF).js
+++ 7. JavaScript Classes - Programação Orientada a Objetos - POO/5. Usando classes (Validando CPF).js
@@ -1,24 +1,55 @@
-class ValidaCpf {
-    constructor(cpfUsuario) {
-        Object.defineProperty(this, 'cpfLimpo', {
-            writable: false,
-            enumerable: true,
-            configurable: false,
-            value: cpfUsuario.replace(/\D+/g, '')
-        })
+// 705.484.450-52 070.987.720-03
+class ValidaCPF {
+    constructor(cpfEnviado) {
+      Object.defineProperty(this, 'cpfLimpo', {
+        writable: false,
+        enumerable: true,
+        configurable: false,
+        value: cpfEnviado.replace(/\D+/g, '')
+      });
     }
-
-    eSequencia() {
-        return this.cpfLimpo.chatAt(0).repeat(11) === this.cpfLimpo
+  
+    éSequência() {
+      return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
     }
-
+  
+    geraNovoCpf() {
+      const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+      const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
+      const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
+      this.novoCPF = cpfSemDigitos + digito1 + digito2;
+    }
+  
+    static geraDigito(cpfSemDigitos) {
+      let total = 0;
+      let reverso = cpfSemDigitos.length + 1;
+  
+      for(let stringNumerica of cpfSemDigitos) {
+        total += reverso * Number(stringNumerica);
+        reverso--;
+      }
+  
+      const digito = 11 - (total % 11);
+      return digito <= 9 ? String(digito) : '0';
+    }
+  
     valida() {
-        if(!this.cpfLimpo) return false
-        if(typeof this.cpfLimpo !== 'string') return false
-        if(this.cpfLimpo.lenght !== 11) return false
-        return 'Cheguei aqui!'
+      if(!this.cpfLimpo) return false;
+      if(typeof this.cpfLimpo !== 'string') return false;
+      if(this.cpfLimpo.length !== 11) return false;
+      if(this.éSequência()) return false;
+      this.geraNovoCpf();
+  
+      return this.novoCPF === this.cpfLimpo;
     }
-}
-
-const validaCpf = new ValidaCpf('024.225.820-44')
-console.log(validaCpf.valida())
+  }
+  
+  let validacpf = new ValidaCPF('070.987.720-03');
+  // validacpf = new ValidaCPF('999.999.999-99');
+  
+  if (validacpf.valida()) {
+    console.log('CPF válido');
+  } else {
+    console.log('CPF inválido');
+  }
+  

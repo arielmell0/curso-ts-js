ad75646c60d628c8e05bd9a2aa50d284afd5e4d0
arielmell0 <arielmello@rede.ulbra.br>
2021-12-17 21:23:53 -0300

    feat: método valida

--- 7. JavaScript Classes - Programação Orientada a Objetos - POO/5. Usando classes (Validando CPF).js
+++ 7. JavaScript Classes - Programação Orientada a Objetos - POO/5. Usando classes (Validando CPF).js
@@ -7,7 +7,14 @@ class ValidaCpf {
             value: cpfUsuario.replace(/\D+/g, '')
         })
     }
+
+    valida() {
+        if(!this.cpfLimpo) return false
+        if(typeof this.cpfLimpo !== 'string') return false
+        if(this.cpfLimpo.lenght !== 11) return false
+        return 'Cheguei aqui!'
+    }
 }
 
-const validaCpf = new ValidaCpf('024.225.820.44')
-console.log(validaCpf)
+const validaCpf = new ValidaCpf('024.225.820-44')
+console.log(validaCpf.valida())

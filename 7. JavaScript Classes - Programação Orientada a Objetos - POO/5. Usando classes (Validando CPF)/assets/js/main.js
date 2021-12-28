cdbb40bbcb21d126d68b3fa30a5d52afddcfb4f0
arielmell0 <arielmello@rede.ulbra.br>
2021-12-27 21:22:50 -0300

    feat: método que limpa o cpf

--- 7. JavaScript Classes - Programação Orientada a Objetos - POO/5. Usando classes (Validando CPF)/assets/js/main.js
+++ 7. JavaScript Classes - Programação Orientada a Objetos - POO/5. Usando classes (Validando CPF)/assets/js/main.js
@@ -1,17 +1,37 @@
 class ValidaFormulario {
-    static limpaCpf(cpf) {
-        const arrayCpf = [...cpf]
-        let arrayLimpo = ''
+    constructor(cpf, nome, sobrenome, usuario, senha, repetirSenha) {
+        this.cpf = cpf
+        this.nome = nome
+        this.sobrenome = sobrenome
+        this.usuario = usuario
+        this.senha = senha
+        this.repetirSenha = repetirSenha
+    }
+
+    limpaCpf() {
+        const arrayCpf = [...this.cpf]
+        let arrayLimpo = new Array()
 
         for(let i = 0; i < 14; i++) {
             if(arrayCpf[i] !== '.' && arrayCpf[i] !== '-') {
-                arrayLimpo += arrayCpf[i]
+                arrayLimpo.push(arrayCpf[i])
             }
         }
 
-        console.log(arrayLimpo)
+        return arrayLimpo
+    }
+
+    static validaCpf(cpfLimpo) {
+        
     }
 }
 
-ValidaFormulario.limpaCpf('024.225.820-44')
-// 02422582044
+const usuario1 = new ValidaFormulario('024.225.820-44', 'Ariel', 'Oliveira', 'coxinha123', 'coxinha123')
+console.log(usuario1.limpaCpf())
+// retorna o array sem pontos e traços 
+// output ->
+// [
+//     '0', '2', '4', '2',
+//     '2', '5', '8', '2',
+//     '0', '4', '4'
+// ]

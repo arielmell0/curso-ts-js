7f1f19a3290c5ff69882a9aec107ad812744b465
arielmell0 <arielmello@rede.ulbra.br>
2021-12-28 22:01:08 -0300

    feat: classe valida cpf

--- 7. JavaScript Classes - Programação Orientada a Objetos - POO/5. Usando classes (Validando CPF)/assets/js/main.js
+++ 7. JavaScript Classes - Programação Orientada a Objetos - POO/5. Usando classes (Validando CPF)/assets/js/main.js
@@ -6,32 +6,47 @@ class ValidaFormulario {
         this.usuario = usuario
         this.senha = senha
         this.repetirSenha = repetirSenha
-    }
 
-    limpaCpf() {
-        const arrayCpf = [...this.cpf]
-        let arrayLimpo = new Array()
-
-        for(let i = 0; i < 14; i++) {
-            if(arrayCpf[i] !== '.' && arrayCpf[i] !== '-') {
-                arrayLimpo.push(arrayCpf[i])
+        Object.defineProperty(this, 'cpfLimpo', {
+            enumerable: true,
+            get: function() {
+              return cpf.replace(/\D+/g, '');
             }
-        }
-
-        return arrayLimpo
+        });
     }
 
-    static validaCpf(cpfLimpo) {
-        
+    validaCpf() {
+        if(typeof this.cpfLimpo === 'undefined') return false;
+        if(this.cpfLimpo.length !== 11) return false;
+        if(this.eSequencia()) return false;
+    
+        const cpfParcial = this.cpfLimpo.slice(0, -2);
+        const digito1 = this.criaDigito(cpfParcial);
+        const digito2 = this.criaDigito(cpfParcial + digito1);
+    
+        const novoCpf = cpfParcial + digito1 + digito2;
+        return novoCpf === this.cpfLimpo;
+    }
+
+    criaDigito(cpfParcial) {
+        const cpfArray = Array.from(cpfParcial);
+  
+        let regressivo = cpfArray.length + 1;
+        const total = cpfArray.reduce((ac, val) => {
+          ac += (regressivo * Number(val));
+          regressivo--;
+          return ac;
+        }, 0);
+      
+        const digito = 11 - (total % 11);
+        return digito > 9 ? '0' : String(digito);
+    }
+
+    eSequencia() {
+        const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+        return sequencia === this.cpfLimpo;
     }
 }
 
-const usuario1 = new ValidaFormulario('024.225.820-44', 'Ariel', 'Oliveira', 'coxinha123', 'coxinha123')
-console.log(usuario1.limpaCpf())
-// retorna o array sem pontos e traços 
-// output ->
-// [
-//     '0', '2', '4', '2',
-//     '2', '5', '8', '2',
-//     '0', '4', '4'
-// ]
+const cpf = new ValidaFormulario('024.225.820-44')
+console.log(cpf.validaCpf())

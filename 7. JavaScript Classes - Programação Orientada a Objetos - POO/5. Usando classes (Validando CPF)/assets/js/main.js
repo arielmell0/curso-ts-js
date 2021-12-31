84154c86defa03b5960bf091da003430db942e9a
arielmell0 <arielmello@rede.ulbra.br>
2021-12-30 21:09:34 -0300

    feat: metodo evento adicionado
    adiciona escuta os eventos e chama o metodo handle para lidar com eles

--- 7. JavaScript Classes - Programação Orientada a Objetos - POO/5. Usando classes (Validando CPF)/assets/js/main.js
+++ 7. JavaScript Classes - Programação Orientada a Objetos - POO/5. Usando classes (Validando CPF)/assets/js/main.js
@@ -3,4 +3,10 @@ class ValidaFormulario {
         this.formulario = document.querySelector('.formulario')
         this.eventos()
     }
+
+    eventos() {
+        this.formulario.addEventListener('submit', e => {
+            this.handleSubmit(e)
+        })
+    }
 }

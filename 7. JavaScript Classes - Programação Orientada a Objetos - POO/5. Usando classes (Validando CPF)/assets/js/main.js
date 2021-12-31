b59ebf1a8caff28338f0b1bd1050bfd4218e1636
arielmell0 <arielmello@rede.ulbra.br>
2021-12-30 21:08:29 -0300

    feat: validacao de formulario

--- 7. JavaScript Classes - Programação Orientada a Objetos - POO/5. Usando classes (Validando CPF)/assets/js/main.js
+++ 7. JavaScript Classes - Programação Orientada a Objetos - POO/5. Usando classes (Validando CPF)/assets/js/main.js
@@ -1,14 +1,6 @@
 class ValidaFormulario {
-    constructor(cpf, nome, sobrenome, usuario, senha, repetirSenha) {
-        this.cpf = cpf
-        this.nome = nome
-        this.sobrenome = sobrenome
-        this.usuario = usuario
-        this.senha = senha
-        this.repetirSenha = repetirSenha
-
+    constructor() {
+        this.formulario = document.querySelector('.formulario')
+        this.eventos()
     }
 }
-
-const cpf = new ValidaFormulario('024.225.820-44')
-console.log(cpf.validaCpf())

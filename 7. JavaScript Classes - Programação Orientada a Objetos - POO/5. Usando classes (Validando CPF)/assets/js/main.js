fdf48e767ae39f71e1505cf2d76d0d709fcc9bbc
arielmell0 <arielmello@rede.ulbra.br>
2021-12-30 23:35:02 -0300

    feat: método camposSaoValidos

--- 7. JavaScript Classes - Programação Orientada a Objetos - POO/5. Usando classes (Validando CPF)/assets/js/main.js
+++ 7. JavaScript Classes - Programação Orientada a Objetos - POO/5. Usando classes (Validando CPF)/assets/js/main.js
@@ -20,4 +20,31 @@ class ValidaFormulario {
             this.formulario.submit()
         }
     }
+
+    camposSaoValidos() {
+        let valid = true
+
+        for(let errorText of this.formulario.querySelectorAll('.error-text')) {
+            errorText.remove()
+        }
+
+        for(let campo of this.formulario.querySelectorAll('.validar')) {
+            const label = campo.previousElementSibling.innerText
+
+            if(!campo.value) {
+                this.criaErro(campo, `Campo "${label}" não pode estar em branco.`)
+                valid = false
+            }
+
+            if(campo.classList.contains('cpf')) {
+                if(!this.validaCPF(campo)) valid = false
+            }
+
+            if(campo.classList.contains('usuario')) {
+                if(!this.validaUsuario(campo)) valid = false
+            }
+        }
+
+        return valid
+    }
 }

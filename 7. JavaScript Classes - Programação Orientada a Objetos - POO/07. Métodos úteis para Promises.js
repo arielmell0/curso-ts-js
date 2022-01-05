0f92fe2f461a3c3a8f3a3a51ccdd9f948fc1c862
arielmell0 <arielmello@rede.ulbra.br>
2022-01-04 23:38:01 -0300

    feat: métodos promises
    Promise.all(array)

---
+++ 7. JavaScript Classes - Programação Orientada a Objetos - POO/07. Métodos úteis para Promises.js
@@ -0,0 +1,43 @@
+function rand(max, min) {
+    max *= 1000
+    min *= 1000
+
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        if(typeof msg !== 'string') reject(new Error())
+
+        setTimeout(() => {
+            resolve(msg.toUpperCase() + ' - Passei na promise')
+        }, tempo)
+    })
+}
+
+// Métodos para promisses
+// Método: Promise.all (resolve todas as Promises)
+const promises = [
+    'Primeiro valor',
+    esperaAi('Promise 1', 3000),
+    esperaAi('Promise 2', 1000),
+    esperaAi('Promise 3', 3000),
+    'Outro valor'
+]
+
+Promise.all(promises)
+    .then(function(valor) {
+        console.log(valor)
+    })
+    .catch(function(erro) {
+        console.log(erro)
+    })
+
+// output ->
+// [
+//   'Primeiro valor',
+//   'PROMISE 1 - Passei na promise',
+//   'PROMISE 2 - Passei na promise',
+//   'PROMISE 3 - Passei na promise',
+//   'Outro valor'
+// ]

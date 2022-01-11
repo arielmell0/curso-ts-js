a08f173dab9affa7bd880a85fee2041f6effb901
arielmell0 <arielmello@rede.ulbra.br>
2022-01-10 23:55:34 -0300

    feat: xhr completo

--- 8. JavaScript assíncrono/4. XMLHttpRequest (GET) + Promises/assets/js/main.js
+++ 8. JavaScript assíncrono/4. XMLHttpRequest (GET) + Promises/assets/js/main.js
@@ -5,9 +5,40 @@ const request = obj => {
 
     xhr.addEventListener('load', () => {
         if(xhr.status >= 200 && xhr.status < 300) {
-            obj.sucess(xhr.responseText)
+            obj.success(xhr.responseText)
         } else {
             obj.error(xhr.statusText)
         }
     })
+}
+
+document.addEventListener('click', evento => {
+    const element = evento.target
+    const tag = element.tagName.toLowerCase()
+
+    if(tag == 'a') {
+        evento.preventDefault()
+        carregaPagina(element)
+    }
+})
+
+function carregaPagina(element) {
+    const href = element.getAttribute('href')
+    const objConfig = {
+        method: 'GET',
+        url: href,
+        success(response) {
+            carregaResultado(response)
+        }, 
+        error(errorText) {
+            console.log(errorText)
+        }
+    }
+
+    request(objConfig)
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado')
+    resultado.innerHTML = response
 }

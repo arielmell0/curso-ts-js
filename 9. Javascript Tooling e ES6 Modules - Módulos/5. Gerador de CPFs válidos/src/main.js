293cd3f5ab705e65ab6e5a563314a25db26ae48e
arielmell0 <arielmello@rede.ulbra.br>
2022-01-21 23:23:57 -0300

    feat: usando uma invocação que se auto invoca para iniciar a validação

--- 9. Javascript Tooling e ES6 Modules - Módulos/5. Gerador de CPFs válidos/src/main.js
+++ 9. Javascript Tooling e ES6 Modules - Módulos/5. Gerador de CPFs válidos/src/main.js
@@ -1,2 +1,8 @@
 import './assets/css/style.css';
 import { GeraCPF } from './modules/GeraCPF';
+
+(function() {
+    const gera = new GeraCPF()
+    const cpfGerado = document.querySelector('.gera-cpf')
+    cpfGerado.innerHTML = gera.geraNovoCpf()
+})()

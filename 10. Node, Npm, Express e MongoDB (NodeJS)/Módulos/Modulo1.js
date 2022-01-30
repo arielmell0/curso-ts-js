7b649b2cb8e2f81ef4d10cc142a6d3955f7fb7be
arielmell0 <arielmello@rede.ulbra.br>
2022-01-30 18:04:56 -0300

    feat: importando os modulos do node

--- 10. Node, Npm, Express e MongoDB (NodeJS)/Módulos/Modulo1.js
+++ 10. Node, Npm, Express e MongoDB (NodeJS)/Módulos/Modulo1.js
@@ -1,9 +1,7 @@
 const nome = 'Ariel'
 const sobrenome = 'Oliveira de Mello'
 
-const falaNome = () => {
-    console.log(nome, sobrenome)
-};
+const falaNome = () => `${nome} ${sobrenome}`
 
 // module.exports.nome = nome
 // module.exports.sobrenome = sobrenome
@@ -11,31 +9,7 @@ const falaNome = () => {
 
 exports.nome = nome
 exports.sobrenome = sobrenome
-exports.falaNome - falaNome // referência
+exports.falaNome = falaNome // referência
 
 // ou usando somente o this
 this.qualquerCoisa = 'O que eu quiser exportar'
-
-console.log(module)
-// Output ->
-// Module {
-//     id: '.',
-//     path: 'c:\\Users\\Ariel\\Documents\\curso-ts-js\\10. Node, Npm, Express e MongoDB (NodeJS)\\Módulos',
-//     exports: {
-//       nome: 'Ariel',
-//       sobrenome: 'Oliveira de Mello',
-//       qualquerCoisa: 'O que eu quiser exportar'
-//     },
-//     filename: 'c:\\Users\\Ariel\\Documents\\curso-ts-js\\10. Node, Npm, Express e MongoDB (NodeJS)\\Módulos\\Modulo1.js',
-//     loaded: false,
-//     children: [],
-//     paths: [
-//       'c:\\Users\\Ariel\\Documents\\curso-ts-js\\10. Node, Npm, Express e MongoDB (NodeJS)\\Módulos\\node_modules',
-//       'c:\\Users\\Ariel\\Documents\\curso-ts-js\\10. Node, Npm, Express e MongoDB (NodeJS)\\node_modules',
-//       'c:\\Users\\Ariel\\Documents\\curso-ts-js\\node_modules',
-//       'c:\\Users\\Ariel\\Documents\\node_modules',
-//       'c:\\Users\\Ariel\\node_modules',
-//       'c:\\Users\\node_modules',
-//       'c:\\node_modules'
-//     ]
-//   }
---
+++ 10. Node, Npm, Express e MongoDB (NodeJS)/Módulos/app.js
@@ -0,0 +1,6 @@
+const mod1 = require('./modulo1')
+
+console.log(mod1.falaNome())
+
+// Output ->
+// Ariel Oliveira de Mello

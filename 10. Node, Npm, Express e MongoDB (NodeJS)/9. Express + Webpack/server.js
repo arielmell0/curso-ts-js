de84f103a60a9a3cda63ca085854e737b86de21e
arielmell0 <arielmello@rede.ulbra.br>
2022-02-13 21:10:48 -0300

    feat: modelo express agora com webpack
    adicionando o webpack ao nosso modelo

---
+++ 10. Node, Npm, Express e MongoDB (NodeJS)/9. Express + Webpack/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const routes = require('./routes')
+const path = require('path')
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+app.set('views', path.resolve(__dirname, 'src', 'views'))
+app.set('view engine', 'ejs')
+
+app.use(routes)
+
+app.listen(3030, () => {
+    console.log('O servidor está rodando na porta 3030!')
+})
